0f813de723e0667919c03efbd2969dd922b88446
Iñaki Ronda Martinez <ronda_10_9@hotmail.com>
2021-01-22 14:28:43 +0100

    commit inicial

---
+++ src/Chat.Common/Entities/ChatMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chat.Common.Entities
+{
+    public class ChatMessage
+    {
+        [Key]
+        public int IdChatMessage { get; set; }
+
+        [StringLength(15)]
+        public string Author { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ src/Chat.Common/Entities/ChatUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chat.Common.Entities
+{
+    public class ChatUser
+    {
+        [Key]
+        public int IdUser { get; set; }
+
+        [StringLength(15)]
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+    }
+}
